38985397c141eee54896020d65668626b43e6843
ovattsuG <gustaalves32@gmail.com>
2023-09-24 12:56:15 -0300

    7DaysOfCode

---
+++ Dia-04_maisLoops_e_randomização/SorteioDeNumeros.js
@@ -0,0 +1,15 @@
+function getRandomArbitrary(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+let i = 0;
+while (i <= 1) {
+    const perguntar = prompt("Tente adivinhar o numero do sortei em duas tentativas, qual valor você deseja chutar ?");
+    if (perguntar == getRandomArbitrary) {
+        console.log("Você adivinhou o numero sorteado !!!");
+        i = i + 2;
+    } else {
+        console.log("você não adivinhou, você tem mais uma tentativa!");
+        i = i + 1;
+    }
+}
+console.log(getRandomArbitrary(0, 10));
